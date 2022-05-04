a2a3b267d961914ffabe290bbc577529a3ba6cdc
Jason Knoll <jasonknoll98@aol.com>
2022-05-04 13:11:49 -0400

    added reqs header to main file

--- README.md
+++ README.md
@@ -3,9 +3,12 @@ Database application for Professor Allen's Intro to Data Engineering final group
 
 ## Installation
 ---
-_Python 3.9.* is recommended_
+_Python 3.9.12 was used for this project_
 
+To install all the required Python packages: 
 `pip install -r reqs.txt`
+
 
 ## How to use
 ---
+This application is composed of two different systems: the text interface and the API which connects to the database. 
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 '''
- **PLEASE READ THE README FILE FIRST**
+ **PLEASE READ THE README FILE TO SEE INSTRUCTIONS**
 
 
  main.py - FGCU Data Engineering Internship DB Project
@@ -7,6 +7,19 @@
  *WE ONLY HAVE A TEXT INTERFACE*
 
  Jason Knoll, Jordan Tatum, Chance Mullen
+
+ Requirements:
+ - A student can register to enter or search internships
+ - A student must provide name and email address when registering
+ - A student can provide an address and phone number when registering
+ - A student does not have to have an internship as they may just register to search for internships
+ - A new internship can be created without a student
+ - An internship has 1 company
+ - A company can have 1 or more internships 
+ - There may be multiple instances of an internship by student and dates (start – end)
+ - An instance of an internship must have 1 student
+ - An internship must have at least one category, and can have multiple categories
+ - A new internship instance should start with a copy of the internship categories 
 '''
 
 import requests as r
